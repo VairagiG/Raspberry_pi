8f493065ad6f638a2d98b9b6efb69c971411925b
Manu <mnyadav98@gmail.com>
2022-12-04 23:06:53 +0530

    Added Read temperature and humidity file without using OOP

--- TempPi_DHT22.py
+++ TempPi_DHT22.py
@@ -3,23 +3,9 @@ import Adafruit_DHT
 DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN = 4
 
-class DHT22:
-    def __init__(self):
-        self.temperature
-        self.humidity
-    
-    def read_temperature(self):
-        self.temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)[1]
-        return self.temperature
+humidity , temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
 
-    def read_humidity(self):
-        self.humidity = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)[0]
-        return self.humidity
-
-def main():
-    # print("Temperature is {0:0.1f}*C ,and Humidity is {1:0.1f}%".format(temp,humid))
-    print(f'Temperature is {DHT22.read_temperature}*C ,and Humidity is {DHT22.read_humidity}%')
-    # return temp, humid
-
-if __name__ == "__main__":
-    main()
+if humidity is not None and temperature is not None:
+    print("Temperature = {0:0.1f}*C and Humidity = {1:0.1f}%".format(temperature,humidity))
+else:
+    print("Failed to retrive data from humidity sensor")
